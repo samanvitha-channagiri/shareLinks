28d17533f6e9cfed21dab1a2848e08f3f246ba86
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-09-19 16:33:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+  import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/utils/LinkData.jsx
@@ -0,0 +1,31 @@
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GithubIcon from '@mui/icons-material/Github';
+import XIcon from '@mui/icons-material/X'
+import WorkIcon from '@mui/icons-material/Work';
+export const LinkData=[
+    {
+        "name":"Github",
+        "url":"https://github.com/samanvitha-channagiri",
+         "IconComponent":<GithubIcon/>
+
+    },
+     {
+        "name":"Portfolio",
+        "url":"https://samanvitha-c.vercel.app",
+         "IconComponent":<WorkIcon/>
+
+    },
+    {
+        "name":"Linkedin",
+        "url":"https://www.linkedin.com/in/samanvitha-c/",
+         "IconComponent":<LinkedInIcon/>
+
+    },
+    {
+        "name":"X",
+        "url":"https://github.com/samanvitha-channagiri",
+         "IconComponent":<XIcon/>
+
+    },
+]
