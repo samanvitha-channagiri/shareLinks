a3643b9a6738d2d572a8d8cfdd53ce4c3491383b
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-09-19 19:10:24 +0530

    changed react version

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-  import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Card from './components/Card';
--- src/utils/LinkData.jsx
+++ src/utils/LinkData.jsx
@@ -1,7 +1,7 @@
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GithubIcon from '@mui/icons-material/Github';
-import XIcon from '@mui/icons-material/X'
+
 import WorkIcon from '@mui/icons-material/Work';
 export const LinkData=[
     {
