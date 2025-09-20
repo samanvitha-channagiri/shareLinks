0d1297c7a623b0803841427df9c630a4652cbeb1
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-09-20 17:51:24 +0530

    added margin on top

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from './components/Card';
 function App() {
   return (
     <div className='h-screen bg-black overflow-hidden'>
-      <div className='flex flex-col items-center max-w-md mx-auto py-[20px] h-full'>
+      <div className='flex flex-col items-center max-w-md mx-auto py-[30px] h-full'>
         <Header/>
         <Card/>
       </div>
